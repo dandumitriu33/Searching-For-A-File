e48e16c683332ebe5209c9a8a2623ce6f7a048d8
Dan Dumitriu <dandumitriu33@gmail.com>
2020-06-28 09:15:17 +0300

    Add search in all subdirectories feature

--- SearchingForAFile/SearchingForAFile/Program.cs
+++ SearchingForAFile/SearchingForAFile/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,25 @@ namespace SearchingForAFile
     {
         static void Main(string[] args)
         {
+            string path = args[1];
+            DirectoryInfo di = new DirectoryInfo(path);
+            try
+            {
+                if (di.Exists)
+                {
+                    Console.WriteLine("The path is valid.");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"The process failed: {e.Message}");
+            }
+            //string[] searchResults = di.GetFiles(path, "*buzz*", SearchOption.AllDirectories);
+            // https://docs.microsoft.com/en-us/dotnet/api/system.io.directory.getfiles?view=netcore-3.1#System_IO_Directory_GetFiles_System_String_System_String_System_IO_SearchOption_
+
+            var searchResults = di.GetFiles("*rel*", SearchOption.AllDirectories);
+            Console.WriteLine(searchResults[0].FullName);
+
         }
     }
 }
