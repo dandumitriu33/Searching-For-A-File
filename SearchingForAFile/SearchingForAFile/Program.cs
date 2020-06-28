449409bab55d5b2fefc5b1c1da0bdfafe21137e3
Dan Dumitriu <dandumitriu33@gmail.com>
2020-06-28 10:25:04 +0300

    Add compression/archiving of files

--- SearchingForAFile/SearchingForAFile/Program.cs
+++ SearchingForAFile/SearchingForAFile/Program.cs
@@ -4,14 +4,17 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
+using System.IO.Compression;
 
 namespace SearchingForAFile
 {
     class Program
     {
+        private static string path { get; set; }
+        public static string zipPath { get; set; }
         static void Main(string[] args)
         {
-            string path = args[1];
+            path = args[1];
             DateTime lastModification = new DateTime(1961, 1, 1, 8, 0, 0);
             DirectoryInfo di = new DirectoryInfo(path);
             try
@@ -34,12 +37,43 @@ namespace SearchingForAFile
                 if (searchResults[0].LastWriteTime > lastModification)
                 {
                     lastModification = searchResults[0].LastWriteTime;
+                    //var fileInfo = new FileInfo(searchResults[0].FullName);
+                    //string newDirectoryName = fileInfo.DirectoryName;
+                    string newDirectoryName = searchResults[0].DirectoryName;
+                    DirectoryInfo di2 = new DirectoryInfo(newDirectoryName);
+                    zipPath = di2.FullName;
+                    Compress(di2);
+                    
                 }
                 Console.WriteLine($"{searchResults[0].Name} was last modified on: {lastModification}");
                 Thread.Sleep(3000);
             }
-            
 
+
+        }
+
+        public static void Compress(DirectoryInfo directorySelected)
+        {
+            foreach (FileInfo fileToCompress in directorySelected.GetFiles())
+            {
+                using (FileStream originalFileStream = fileToCompress.OpenRead())
+                {
+                    if ((File.GetAttributes(fileToCompress.FullName) &
+                       FileAttributes.Hidden) != FileAttributes.Hidden & fileToCompress.Extension != ".gz")
+                    {
+                        using (FileStream compressedFileStream = File.Create(fileToCompress.FullName + ".gz"))
+                        {
+                            using (GZipStream compressionStream = new GZipStream(compressedFileStream,
+                               CompressionMode.Compress))
+                            {
+                                originalFileStream.CopyTo(compressionStream);
+                            }
+                        }
+                        FileInfo info = new FileInfo(zipPath + Path.DirectorySeparatorChar + fileToCompress.Name + ".gz");
+                        Console.WriteLine($"Compressed {fileToCompress.Name}");
+                    }
+                }
+            }
         }
     }
 }
